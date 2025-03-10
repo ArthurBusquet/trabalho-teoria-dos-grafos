de7f9ad0a2bd338b18b20bb4031ec70225e56210
Jorge Carvalho <116168209+Jorgecvr@users.noreply.github.com>
2025-03-10 21:37:58 +0000

    feat: cria nova leitura de arquivo

--- include/Grafo.h
+++ include/Grafo.h
@@ -7,7 +7,7 @@ using namespace std;
 
 /**
  * @brief Classe base para a representação de um grafo.
- * 
+ *
  * Essa classe define as operações gerais que podem ser realizadas em grafos,
  * como manipulação de arestas, vértices, grau, e conexões. As subclasses precisam
  * implementar a inicialização do grafo e operações de manipulação específicas.
@@ -23,11 +23,76 @@ private:
     int destino;      /**< Vértice de destino para operações com arestas */
     int peso;         /**< Peso das arestas ou vértices (quando ponderados) */
 
+    const int MAX_NODES = 50000;
+    const int MAX_EDGES = 1000000;
+
+    struct NodeMapping
+    {
+        int original;
+        int mapped;
+    };
+
+    int mapeia_vertice(NodeMapping node_mapping[], int &node_count, int node, int &next_id)
+    {
+        for (int i = 0; i < node_count; i++)
+        {
+            if (node_mapping[i].original == node)
+            {
+                return node_mapping[i].mapped; // Retorna o ID já mapeado
+            }
+        }
+        node_mapping[node_count].original = node;
+        node_mapping[node_count].mapped = next_id;
+        set_vertice(next_id, 1);
+        node_count++;
+        return next_id++; // Retorna o novo ID e incrementa o próximo ID
+    }
+
 public:
     /**
      * @brief Construtor padrão.
      */
     Grafo() = default;
+
+    void carrega_grafo_novo()
+    {
+
+        std::string filename = "./entradas/aves-weaver-social-14.edges";
+
+        // Abre o arquivo para leitura
+        std::ifstream file(filename);
+        if (!file.is_open())
+        {
+            std::cerr << "Erro ao abrir o arquivo " << filename << std::endl;
+            return;
+        }
+
+        set_eh_direcionado(false);
+        set_aresta_ponderada(false);
+        set_vertice_ponderado(false);
+
+        NodeMapping node_mapping[MAX_NODES];
+        int node_count = 0; // Contador de nós mapeados
+        int edge_count = 0; // Contador de arestas
+        int next_id = 1;    // Próximo ID sequencial a ser atribuído
+
+        int node1, node2;
+
+        // Lê o arquivo linha por linha
+        while (file >> node1 >> node2)
+        {
+
+            int mapped_node1 = mapeia_vertice(node_mapping, node_count, node1, next_id);
+            int mapped_node2 = mapeia_vertice(node_mapping, node_count, node2, next_id);
+
+            set_aresta(mapped_node1, mapped_node2, 1);
+            edge_count++;
+        }
+
+        file.close();
+
+        set_ordem(node_count);
+    };
 
     /**
      * @brief Destruidor virtual.
@@ -36,7 +101,7 @@ public:
 
     /**
      * @brief Método abstrato para obter o peso de uma aresta entre dois vértices.
-     * 
+     *
      * @param origem Vértice de origem da aresta.
      * @param destino Vértice de destino da aresta.
      * @return O peso da aresta.
@@ -45,7 +110,7 @@ public:
 
     /**
      * @brief Método abstrato para obter o peso de um vértice.
-     * 
+     *
      * @param vertice O identificador do vértice.
      * @return O peso do vértice.
      */
@@ -53,7 +118,7 @@ public:
 
     /**
      * @brief Método abstrato para obter o número de vizinhos de um vértice.
-     * 
+     *
      * @param vertice O identificador do vértice.
      * @return O número de vizinhos.
      */
@@ -61,7 +126,7 @@ public:
 
     /**
      * @brief Método abstrato para adicionar uma nova aresta entre dois vértices.
-     * 
+     *
      * @param origem Vértice de origem da aresta.
      * @param destino Vértice de destino da aresta.
      * @param peso Peso da aresta.
@@ -70,7 +135,7 @@ public:
 
     /**
      * @brief Método abstrato para definir o peso de uma aresta.
-     * 
+     *
      * @param origem Vértice de origem da aresta.
      * @param destino Vértice de destino da aresta.
      * @param peso Peso da aresta.
@@ -79,7 +144,7 @@ public:
 
     /**
      * @brief Método abstrato para definir o peso de um vértice.
-     * 
+     *
      * @param id Identificador do vértice.
      * @param peso Peso do vértice.
      */
@@ -87,7 +152,7 @@ public:
 
     /**
      * @brief Obtém o número de vértices no grafo (ordem do grafo).
-     * 
+     *
      * @return O número de vértices do grafo.
      */
     int get_ordem()
@@ -97,7 +162,7 @@ public:
 
     /**
      * @brief Define o número de vértices do grafo.
-     * 
+     *
      * @param ordem O número de vértices a ser definido.
      */
     void set_ordem(int ordem)
@@ -115,7 +180,7 @@ public:
 
     /**
      * @brief Verifica se o grafo é direcionado.
-     * 
+     *
      * @return Verdadeiro se o grafo for direcionado, falso caso contrário.
      */
     bool eh_direcionado()
@@ -125,7 +190,7 @@ public:
 
     /**
      * @brief Define se o grafo é direcionado.
-     * 
+     *
      * @param direcionado Valor que define se o grafo será direcionado.
      */
     void set_eh_direcionado(bool direcionado)
@@ -135,7 +200,7 @@ public:
 
     /**
      * @brief Verifica se os vértices do grafo são ponderados.
-     * 
+     *
      * @return Verdadeiro se os vértices forem ponderados, falso caso contrário.
      */
     bool vertice_ponderado()
@@ -145,7 +210,7 @@ public:
 
     /**
      * @brief Define se os vértices do grafo são ponderados.
-     * 
+     *
      * @param verticePonderado Valor que define se os vértices serão ponderados.
      */
     void set_vertice_ponderado(bool verticePonderado)
@@ -155,7 +220,7 @@ public:
 
     /**
      * @brief Verifica se as arestas do grafo são ponderadas.
-     * 
+     *
      * @return Verdadeiro se as arestas forem ponderadas, falso caso contrário.
      */
     bool aresta_ponderada()
@@ -165,7 +230,7 @@ public:
 
     /**
      * @brief Define se as arestas do grafo são ponderadas.
-     * 
+     *
      * @param arestaPonderada Valor que define se as arestas serão ponderadas.
      */
     void set_aresta_ponderada(bool arestaPonderada)
@@ -228,7 +293,7 @@ public:
 
         while (arquivo >> origem >> destino >> peso)
         {
-            if(!aresta_ponderada())
+            if (!aresta_ponderada())
                 peso = 0;
             set_aresta(origem, destino, peso);
         }
@@ -236,7 +301,7 @@ public:
 
     /**
      * @brief Obtém o grau (número de vizinhos) do vértice com maior grau.
-     * 
+     *
      * @return O grau máximo.
      */
     int get_grau()
@@ -278,7 +343,7 @@ public:
 
     /**
      * @brief Verifica se o grafo é completo.
-     * 
+     *
      * @return Verdadeiro se o grafo for completo (todos os vértices estão conectados entre si), falso caso contrário.
      */
     bool eh_completo()
@@ -293,14 +358,17 @@ public:
 
     /**
      * @brief Realiza uma busca em profundidade (DFS) para explorar todos os vértices conectados a partir de um vértice inicial.
-     * 
+     *
      * @param vertice Vértice de origem para a busca.
      * @param visitado Array de visitados para marcar os vértices já visitados.
      */
-    void dfs(int vertice, bool visitado[]) {
+    void dfs(int vertice, bool visitado[])
+    {
         visitado[vertice] = true;
-        for (int i = 1; i <= ordem; i++) {
-            if (get_aresta(vertice, i) && !visitado[i]) {
+        for (int i = 1; i <= ordem; i++)
+        {
+            if (get_aresta(vertice, i) && !visitado[i])
+            {
                 dfs(i, visitado);
             }
         }
@@ -308,19 +376,23 @@ public:
 
     /**
      * @brief Calcula o número de componentes conexas do grafo.
-     * 
+     *
      * @return O número de componentes conexas no grafo.
      */
-    int n_conexo() {
-        bool* visitado = new bool[ordem + 1]; // Usa alocação dinâmica para evitar problemas de tamanho
-        for (int i = 1; i <= ordem; i++) { // Se os vértices começam em 1
+    int n_conexo()
+    {
+        bool *visitado = new bool[ordem + 1]; // Usa alocação dinâmica para evitar problemas de tamanho
+        for (int i = 1; i <= ordem; i++)
+        {                        // Se os vértices começam em 1
             visitado[i] = false; // Inicializa corretamente
         }
 
         int componentes = 0;
-        
-        for (int i = 1; i <= ordem; i++) { // Se os vértices começam em 1
-            if (!visitado[i]) { // Usa índice corretamente
+
+        for (int i = 1; i <= ordem; i++)
+        { // Se os vértices começam em 1
+            if (!visitado[i])
+            {                     // Usa índice corretamente
                 dfs(i, visitado); // Chama a DFS
                 componentes++;
             }
@@ -337,15 +409,17 @@ public:
 
     /**
      * @brief Encontra a maior menor distância entre os vértices utilizando o algoritmo de Floyd-Warshall.
-     * 
+     *
      * O algoritmo calcula todas as menores distâncias entre pares de vértices e depois encontra a maior distância
      * entre qualquer par de vértices. Caso não haja caminho entre os vértices, o algoritmo considerará um valor
      * de infinito para aquelas distâncias.
      */
-    void maior_menor_distancia() {
+    void maior_menor_distancia()
+    {
         int n = get_ordem();
 
-        if (n == 0) {
+        if (n == 0)
+        {
             cout << "O grafo está vazio." << endl;
             return;
         }
@@ -354,11 +428,16 @@ public:
         int dist[n + 1][n + 1];
 
         // Inicializa a matriz de distâncias
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (i == j)
+                {
                     dist[i][j] = 0; // Distância de um nó para ele mesmo
-                } else {
+                }
+                else
+                {
                     int peso = get_aresta(i, j);
                     dist[i][j] = (peso > 0) ? peso : 999999; // Se não há aresta, assume um valor alto (infinito)
                 }
@@ -366,11 +445,16 @@ public:
         }
 
         // Algoritmo de Floyd-Warshall
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (dist[i][k] != 999999 && dist[k][j] != 999999) {
-                        if (dist[i][j] > dist[i][k] + dist[k][j]) {
+        for (int k = 1; k <= n; k++)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (dist[i][k] != 999999 && dist[k][j] != 999999)
+                    {
+                        if (dist[i][j] > dist[i][k] + dist[k][j])
+                        {
                             dist[i][j] = dist[i][k] + dist[k][j];
                         }
                     }
@@ -382,9 +466,12 @@ public:
         int maxDist = -1;
         int no1 = -1, no2 = -1;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                if (dist[i][j] != 999999 && dist[i][j] > maxDist) {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = i + 1; j <= n; j++)
+            {
+                if (dist[i][j] != 999999 && dist[i][j] > maxDist)
+                {
                     maxDist = dist[i][j];
                     no1 = i;
                     no2 = j;
@@ -393,9 +480,12 @@ public:
         }
 
         // Exibir resultado
-        if (no1 != -1 && no2 != -1) {
+        if (no1 != -1 && no2 != -1)
+        {
             cout << "Maior menor distância: (" << no1 << "-" << no2 << ") " << maxDist << endl;
-        } else {
+        }
+        else
+        {
             cout << "Não há caminho entre os nós." << endl;
         }
     }
--- src/GrafoMatriz.cpp
+++ src/GrafoMatriz.cpp
@@ -10,13 +10,15 @@ using namespace std;
 
 const int TAMANHO_FIXO = 10000; // Define um tamanho fixo grande para a matriz
 
-GrafoMatriz::GrafoMatriz() {
+GrafoMatriz::GrafoMatriz()
+{
     tamanhoAtual = TAMANHO_FIXO;
     size_t tamanhoAtualLinear = ((size_t)TAMANHO_FIXO * (TAMANHO_FIXO + 1)) / 2;
 
     // Aloca matriz 2D estaticamente
     Matriz = new int*[tamanhoAtual];
-    for (int i = 0; i < tamanhoAtual; i++) {
+    for (int i = 0; i < tamanhoAtual; i++)
+    {
         Matriz[i] = new int[tamanhoAtual](); // Inicializa com zero
     }
 
@@ -27,15 +29,17 @@ GrafoMatriz::GrafoMatriz() {
     VetorPesosVertices = new int[tamanhoAtual](); // Inicializa com zero
 }
 
-GrafoMatriz::~GrafoMatriz() {
+GrafoMatriz::~GrafoMatriz()
+{
     // Libera a memória alocada para a matriz 2D
-    for (int i = 0; i < tamanhoAtual; i++) {
-        delete[] Matriz[i];  // Libera cada linha da matriz
+    for (int i = 0; i < tamanhoAtual; i++)
+    {
+        delete[] Matriz[i]; // Libera cada linha da matriz
     }
-    delete[] Matriz;  // Libera o array de ponteiros para as linhas da matriz
+    delete[] Matriz; // Libera o array de ponteiros para as linhas da matriz
 
     // Libera a memória alocada para a matriz linear
-    delete[] MatrizLinear;  // Libera o vetor que armazena a matriz linear
+    delete[] MatrizLinear; // Libera o vetor que armazena a matriz linear
 
     cout << "Destruindo GrafoMatriz..." << endl;
 }
@@ -85,9 +89,11 @@ GrafoMatriz::~GrafoMatriz() {
 // }
 
 // Método para inicializar o grafo a partir de um arquivo
-void GrafoMatriz::inicializa_grafo() {
+void GrafoMatriz::inicializa_grafo()
+{
     ifstream arquivo("./entradas/Grafo.txt");
-    if (!arquivo.is_open()) {
+    if (!arquivo.is_open())
+    {
         cerr << "Erro ao abrir o arquivo Grafo.txt" << endl;
         return;
     }
@@ -96,135 +102,190 @@ void GrafoMatriz::inicializa_grafo() {
     arquivo >> num_vertices >> direcionado >> ponderado_vertices >> ponderado_arestas;
 
     // Lendo os pesos dos vértices, caso o grafo seja ponderado nos vértices
-    if (ponderado_vertices) {
+    if (ponderado_vertices)
+    {
         int index = 0;
-        while (index < num_vertices) {
+        while (index < num_vertices)
+        {
             arquivo >> VetorPesosVertices[index];
-            index = index + 1;  // Preenche o vetor com os pesos dos vértices
+            index = index + 1; // Preenche o vetor com os pesos dos vértices
         }
     }
 
     // Inicializando a matriz de adjacência
-    if (direcionado) {
+    if (direcionado)
+    {
         int linha = 0;
-        while (linha < num_vertices) {
+        while (linha < num_vertices)
+        {
             int coluna = 0;
-            while (coluna < num_vertices) {
-                Matriz[linha][coluna] = 0;  // Inicializa todas as entradas como zero
+            while (coluna < num_vertices)
+            {
+                Matriz[linha][coluna] = 0; // Inicializa todas as entradas como zero
                 coluna = coluna + 1;
             }
             linha = linha + 1;
         }
-    } else {
+    }
+    else
+    {
         int index = 0;
-        while (index < (num_vertices * (num_vertices + 1)) / 2) {
-            MatrizLinear[index] = 0;  // Inicializa a matriz linear com zero
+        while (index < (num_vertices * (num_vertices + 1)) / 2)
+        {
+            MatrizLinear[index] = 0; // Inicializa a matriz linear com zero
             index = index + 1;
         }
     }
 
     // Lendo as arestas do arquivo
     int origem, destino, peso;
-    while (arquivo >> origem >> destino) {
-        if (ponderado_arestas) {
+    while (arquivo >> origem >> destino)
+    {
+        if (ponderado_arestas)
+        {
             arquivo >> peso;
-        } else {
+        }
+        else
+        {
             arquivo >> peso;
-            peso = 1;  // Arestas não ponderadas têm peso padrão 1
+            peso = 1; // Arestas não ponderadas têm peso padrão 1
         }
 
-        if (direcionado) {
-            Matriz[origem - 1][destino - 1] = peso;  // Adiciona aresta na matriz para grafos direcionados
-        } else {
+        if (direcionado)
+        {
+            Matriz[origem - 1][destino - 1] = peso; // Adiciona aresta na matriz para grafos direcionados
+        }
+        else
+        {
             int indice = calcularIndiceLinear(origem, destino);
-            MatrizLinear[indice] = peso;  // Adiciona aresta na matriz linear para grafos não direcionados
+            MatrizLinear[indice] = peso; // Adiciona aresta na matriz linear para grafos não direcionados
         }
     }
-    arquivo.close();  // Fecha o arquivo
+    arquivo.close(); // Fecha o arquivo
 }
 
 // Método para calcular o índice linear da matriz comprimida
-int GrafoMatriz::calcularIndiceLinear(int origem, int destino) {
-    if (origem <= destino) {
-        return (destino * (destino - 1)) / 2 + origem;  // Calcula índice linear considerando origem e destino
+int GrafoMatriz::calcularIndiceLinear(int origem, int destino)
+{
+    // Garante que origem <= destino
+    if (origem > destino)
+    {
+        std::swap(origem, destino);
     }
-    return (origem * (origem - 1)) / 2 + destino;  // Caso contrário, calcula de forma inversa
+    // Fórmula correta para índice linear em uma matriz triangular inferior
+    return (destino * (destino - 1)) / 2 + origem;
 }
 
 // Método para obter o peso da aresta entre dois vértices
-int GrafoMatriz::get_aresta(int origem, int destino) {
-    if (origem < 1 || destino < 1 || origem > tamanhoAtual || destino > tamanhoAtual) {
+int GrafoMatriz::get_aresta(int origem, int destino)
+{
+    if (origem < 1 || destino < 1 || origem > tamanhoAtual || destino > tamanhoAtual)
+    {
         cerr << "Erro: Índices fora dos limites da matriz!" << endl;
-        return -1;  // Retorno de erro se os índices estiverem fora dos limites
+        return -1; // Retorno de erro se os índices estiverem fora dos limites
     }
 
-    if (eh_direcionado()) {
-        return Matriz[origem - 1][destino - 1];  // Para grafos direcionados, retorna diretamente da matriz
-    } else {
-        if (origem > destino) {
-            std::swap(origem, destino);  // Garante que origem é sempre menor ou igual a destino
+    if (eh_direcionado())
+    {
+        return Matriz[origem - 1][destino - 1]; // Para grafos direcionados, retorna diretamente da matriz
+    }
+    else
+    {
+        if (origem > destino)
+        {
+            std::swap(origem, destino); // Garante que origem é sempre menor ou igual a destino
         }
         int indice = calcularIndiceLinear(origem, destino);
-        return MatrizLinear[indice];  // Para grafos não direcionados, retorna da matriz comprimida
+        return MatrizLinear[indice]; // Para grafos não direcionados, retorna da matriz comprimida
     }
 }
 
 // Método para definir o peso de um vértice
-void GrafoMatriz::set_vertice(int id, float peso) {
-    if(!vertice_ponderado())  // Verifica se o grafo é ponderado
-        peso = 0;  // Se não for ponderado, atribui peso 0
+void GrafoMatriz::set_vertice(int id, float peso)
+{
+    if (!vertice_ponderado()) // Verifica se o grafo é ponderado
+        peso = 0;             // Se não for ponderado, atribui peso 0
 
-    VetorPesosVertices[id - 1] = peso;  // Define o peso do vértice
+    VetorPesosVertices[id - 1] = peso; // Define o peso do vértice
 }
 
 // Método para definir o peso de uma aresta entre dois vértices
-void GrafoMatriz::set_aresta(int origem, int destino, float peso) {
-    if(!aresta_ponderada())  // Verifica se o grafo é ponderado
-        peso = 0;  // Se não for ponderado, atribui peso 0
-    
-    if (eh_direcionado()) {
-        Matriz[origem][destino] = peso;  // Para grafos direcionados, adiciona na matriz de adjacência
-    } else {
+void GrafoMatriz::set_aresta(int origem, int destino, float peso)
+{
+
+    if (!aresta_ponderada()) // Verifica se o grafo é ponderado
+        peso = 1;            // Se não for ponderado, atribui peso 0
+
+    if (eh_direcionado())
+    {
+        Matriz[origem][destino] = peso; // Para grafos direcionados, adiciona na matriz de adjacência
+    }
+    else
+    {
         // Certifique-se de que origem é sempre menor ou igual a destino
-        if (origem > destino) {
-            std::swap(origem, destino);  // Troca origem e destino para garantir a ordem
+        if (origem > destino)
+        {
+            std::swap(origem, destino); // Troca origem e destino para garantir a ordem
         }
         int indice = calcularIndiceLinear(origem, destino);
-        MatrizLinear[indice] = peso;  // Para grafos não direcionados, adiciona na matriz comprimida
+        MatrizLinear[indice] = peso; // Para grafos não direcionados, adiciona na matriz comprimida
+    }
+
+    std::cout << "Matriz Triangular Inferior:" << std::endl;
+
+    for (int i = 0; i < 11; i++) {
+        for (int j = 0; j <= i; j++) {
+            // Calcula o índice linear para a posição (i, j)
+            int indice = calcularIndiceLinear(j, i);
+            std::cout << MatrizLinear[indice] << " ";
+        }
+        std::cout << std::endl;
     }
 }
 
 // Método para obter o peso de um vértice
-int GrafoMatriz::get_vertice(int origem) {
-    if (vertice_ponderado()) {
-        return VetorPesosVertices[origem - 1];  // Retorna o peso do vértice se ponderado
+int GrafoMatriz::get_vertice(int origem)
+{
+    if (vertice_ponderado())
+    {
+        return VetorPesosVertices[origem - 1]; // Retorna o peso do vértice se ponderado
     }
-    return 1;  // Caso não seja ponderado, retorna peso 1 por padrão
+    return 1; // Caso não seja ponderado, retorna peso 1 por padrão
 }
 
 // Método para obter o número de vizinhos de um vértice
-int GrafoMatriz::get_vizinhos(int vertice) {
-    if (vertice < 1 || vertice > get_ordem()) {
+int GrafoMatriz::get_vizinhos(int vertice)
+{
+    if (vertice < 1 || vertice > get_ordem())
+    {
         cerr << "Vértice inválido!" << endl;
-        return -1;  // Retorno de erro se o vértice for inválido
+        return -1; // Retorno de erro se o vértice for inválido
     }
 
     int vizinhos = 0;
-    int v = vertice - 1;  // Ajustando para índice 0
+    int v = vertice - 1; // Ajustando para índice 0
 
-    if (eh_direcionado()) {
+    if (eh_direcionado())
+    {
         // Contar arestas de entrada e saída
-        for (int i = 0; i < get_ordem(); i++) {
-            if (Matriz[v][i] != 0) vizinhos++;  // Contando saídas
+        for (int i = 0; i < get_ordem(); i++)
+        {
+            if (Matriz[v][i] != 0)
+                vizinhos++; // Contando saídas
         }
-    } else {
+    }
+    else
+    {
         // Para grafos não direcionados, verificar matriz comprimida
-        for (int i = 0; i < get_ordem(); i++) {
-            if (i == v) continue;  // Evita contar o próprio vértice
+        for (int i = 0; i < get_ordem(); i++)
+        {
+            if (i == v)
+                continue; // Evita contar o próprio vértice
 
             int indice = calcularIndiceLinear(i + 1, vertice);
-            if (MatrizLinear[indice] != 0) {
-                vizinhos++;  // Conta os vizinhos
+            if (MatrizLinear[indice] != 0)
+            {
+                vizinhos++; // Conta os vizinhos
             }
         }
     }
@@ -233,29 +294,34 @@ int GrafoMatriz::get_vizinhos(int vertice) {
 }
 
 // Método para adicionar uma nova aresta entre dois vértices
-void GrafoMatriz::nova_aresta(int origem, int destino, int peso) {
+void GrafoMatriz::nova_aresta(int origem, int destino, int peso)
+{
     std::cout << "Tentando adicionar aresta entre " << origem << " e " << destino << " com peso " << peso << std::endl;
 
     // Verificando se a aresta já existe
-    if (get_aresta(origem, destino) != 0) {
+    if (get_aresta(origem, destino) != 0)
+    {
         std::cout << "Aresta entre " << origem << " e " << destino << " já existe!" << std::endl;
         return;
     }
 
     // Verifica se os índices estão dentro do limite
-    if (origem > tamanhoAtual || destino > tamanhoAtual) {
+    if (origem > tamanhoAtual || destino > tamanhoAtual)
+    {
         std::cerr << "Erro: Índices de vértice estão fora dos limites da matriz!" << std::endl;
         return;
     }
 
     // Para grafos direcionados
-    if (eh_direcionado()) {
-        Matriz[origem - 1][destino - 1] = peso;  // Adiciona a aresta na matriz de adjacência
+    if (eh_direcionado())
+    {
+        Matriz[origem - 1][destino - 1] = peso; // Adiciona a aresta na matriz de adjacência
         std::cout << "Aresta adicionada na matriz de adjacência!" << std::endl;
-    } 
+    }
     // Para grafos não direcionados
-    else {
+    else
+    {
         int indice = calcularIndiceLinear(origem, destino);
-        MatrizLinear[indice] = peso;  // Adiciona a aresta na matriz comprimida
+        MatrizLinear[indice] = peso; // Adiciona a aresta na matriz comprimida
     }
 }
